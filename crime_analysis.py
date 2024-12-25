bc0c14b45e98ad61f20a1d60f0888f77c6f14093
kochsterex <kochiashvili.giorgi@kiu.edu.ge>
2024-12-25 22:16:39 +0400

    Update crime_analysis.py

--- crime_analysis.py
+++ crime_analysis.py
@@ -3,6 +3,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder, StandardScaler
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import ConfusionMatrixDisplay
 from sklearn.metrics import classification_report, accuracy_score
 import numpy as np
 import matplotlib.pyplot as plt
@@ -97,6 +98,14 @@ def model_weapon_training(X_train, X_test, y_train, y_test):
     print("Weapon prediction - Classification report:")
     print(classification_report(y_test, y_pred_weapon))
     print("Weapon prediction - Accuracy:", accuracy_score(y_test, y_pred_weapon))
+    
+    return model_weapon
+
+def model_weapon_visualize(model_weapon, X_test_weapon, y_test_weapon):
+    # display the confusion matrix that shows how well the model predicts if a weapon was used or not
+    ConfusionMatrixDisplay.from_estimator(model_weapon, X_test_weapon, y_test_weapon)
+    plt.title("Weapon prediction confusion matrix")
+    plt.show()
 
 def model_crime_training(X_train, X_test, y_train, y_test):
     # filter the data to include only the crime types with at least threshold amount of support
@@ -127,9 +136,49 @@ def model_crime_training(X_train, X_test, y_train, y_test):
     print(classification_report(y_test_filtered, y_pred_filtered, zero_division=1, labels=valid_classes))
     print("Crime type prediction - Accuracy:", accuracy_score(y_test_filtered, y_pred_filtered))
 
+    return model_crime_type
 
+def model_crime_visualize(model_crime_type, X_test_crime, y_test_crime):
+    # number of different crime types, because too much will overload the confusion matrix
+    N = 10
+    
+    # get the frequencies of these crime types 
+    unique_types, type_counts = np.unique(y_test_crime, return_counts=True)
 
+    # get the top N crime types
+    top_n_types = unique_types[np.argsort(-type_counts)[:N]]
+    
+    # filter the data and leave only the rows with the crimes that we want
+    mask = np.isin(y_test_crime, top_n_types)
+    X_test_filtered = X_test_crime[mask]
+    y_test_filtered = y_test_crime[mask]
 
+    # predict the crime types
+    y_pred_filtered = model_crime_type.predict(X_test_filtered)
+
+    # make sure the predictions are only from top N crime types
+    mask_pred = np.isin(y_pred_filtered, top_n_types)
+    y_test_filtered = y_test_filtered[mask_pred]
+    y_pred_filtered = y_pred_filtered[mask_pred]
+
+    # map the test and prediction data to integers
+    label_map = {label: idx for idx, label in enumerate(top_n_types)}
+
+    y_test_mapped = np.array([label_map[label] for label in y_test_filtered])
+    y_pred_mapped = np.array([label_map[label] for label in y_pred_filtered])
+    
+    # display the confusion matrix
+    disp = ConfusionMatrixDisplay.from_predictions(
+        y_test_mapped,
+        y_pred_mapped,
+        display_labels=top_n_types,
+        xticks_rotation=45,
+        cmap='Blues'
+    )
+    disp.figure_.set_size_inches(8, 6)
+    plt.title("Crime type prediction confusion matrix (top 10 classes)")
+    plt.tight_layout()
+    plt.show()
 
 def is_truly_numeric_column(column):
     try:
@@ -179,7 +228,7 @@ def plot_visualizations(data):
     # box plot for victim age distribution by top crime types
     plt.figure(figsize=(12, 8))
     top_crimes = data['Crm Cd Desc'].value_counts().head(5).index
-    sns.boxplot(x='Crm Cd Desc', y='Vict Age', data=data[data['Crm Cd Desc'].isin(top_crimes)], palette='Set3')
+    sns.boxplot(x='Crm Cd Desc', y='Vict Age', data=data[data['Crm Cd Desc'].isin(top_crimes)], palette='Set3', hue='Crm Cd Desc', legend=False)
     plt.title('Victim Age Distribution by Top Crime Types')
     plt.xlabel('Crime Type')
     plt.ylabel('Victim Age')
@@ -196,9 +245,11 @@ def plot_visualizations(data):
 
 def main():
     data_filtered, X_train_weapon, X_test_weapon, y_train_weapon, y_test_weapon, X_train_crime, X_test_crime, y_train_crime, y_test_crime = data_preparation()
-    model_weapon_training(X_train_weapon, X_test_weapon, y_train_weapon, y_test_weapon)
-    model_crime_training(X_train_crime, X_test_crime, y_train_crime, y_test_crime)
-    plot_visualizations(data_filtered)
+    model_weapon = model_weapon_training(X_train_weapon, X_test_weapon, y_train_weapon, y_test_weapon)
+    # model_crime_type = model_crime_training(X_train_crime, X_test_crime, y_train_crime, y_test_crime)
+    # plot_visualizations(data_filtered)
+    # model_weapon_visualize(model_weapon, X_test_weapon, y_test_weapon)
+    # model_crime_visualize(model_crime_type, X_test_crime, y_test_crime)
 
 if __name__ == "__main__":
     main()
